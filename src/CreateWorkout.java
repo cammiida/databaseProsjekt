e8f7ae64d649b84b4ede2ccef7245e508a573210
Camilla Marie Dalan <camillamariedalan@dhcp-10-22-37-211.wlan.ntnu.no>
2017-03-14 15:57:24 +0100

    Add createworkout and gitignore.

--- src/CreateWorkout.java
+++ src/CreateWorkout.java
@@ -1,9 +1,56 @@
+
 import java.sql.*;
 
 
 public class CreateWorkout {
 
-    public CreateWorkout (Connection connection){
+    Statement stmt = null;
+    ResultSet rs = null;
+
+    public CreateWorkout(Connection connection){
+
+        try {
+            stmt = connection.createStatement();
+            //rs = stmt.executeQuery("SELECT * from Gruppe");
+
+
+            if (stmt.execute("SELECT * FROM Gruppe")) {
+                rs = stmt.getResultSet();
+            }
+            rs.next();
+            System.out.println(rs.getString("Navn"));
+            System.out.println(rs.getString("Beskrivelse"));
+
+        }
+
+        catch (SQLException ex){
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+        finally {
+            // it is a good idea to release
+            // resources in a finally{} block
+            // in reverse-order of their creation
+            // if they are no-longer needed
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException sqlEx) { } // ignore
+
+                rs = null;
+            }
+
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) { } // ignore
+
+                stmt = null;
+            }
+        }
 
     }
 
